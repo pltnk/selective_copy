b6dda3d0873370dddac298bd41174c753eda4c71
Kirill Plotnikov <kpltnk@gmail.com>
2019-06-06 14:51:17 +0500

    Add license info

--- selective_copy.py
+++ selective_copy.py
@@ -4,6 +4,7 @@
 # Allows to preserve source folder structure and to create a log if necessary.
 # Opens a filedialog if source and/or destination are not given in the command line.
 # Creates folders in destination if they don't exist.
+# This project is licensed under the MIT License.
 # Copyright (c) 2019 Kirill Plotnikov
 
 
