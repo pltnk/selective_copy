1482f6cd6018b654b1492ec7bf635b93b9685fdf
Kirill Plotnikov <kpltnk@gmail.com>
2019-06-06 14:49:33 +0500

    Add copyright

--- selective_copy.py
+++ selective_copy.py
@@ -4,6 +4,7 @@
 # Allows to preserve source folder structure and to create a log if necessary.
 # Opens a filedialog if source and/or destination are not given in the command line.
 # Creates folders in destination if they don't exist.
+# Copyright (c) 2019 Kirill Plotnikov
 
 
 import logging, os, shutil, sys
