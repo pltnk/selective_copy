2da8c675b16f71f9cdbd422bc92286b9ccc2cdee
Kirill Plotnikov <kpltnk@gmail.com>
2019-07-15 16:56:38 +0500

    Update description
    Add information about 'exclude' option

--- slcp.py
+++ slcp.py
@@ -1,7 +1,7 @@
 # slcp.py - command line application that copies all files with given extensions
 # from a directory and its subfolders to another directory.
 # Allows to preserve a source folder structure, to process only files without given extensions,
-# to move files instead of copying and to create a log if necessary.
+# to move files instead of copying, to exclude certain files from processing and to create a log if necessary.
 # Opens a filedialog if source and/or destination are not given in the command line.
 # Creates folders in destination if they don't exist.
 # This project is licensed under the MIT License.
